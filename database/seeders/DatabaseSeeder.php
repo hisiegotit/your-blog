6fc6917f614ea5633c17008fc8883133944c5b8e
hisiegotit <hisie.work@gmail.com>
2023-11-30 11:53:03 +0700

    update DataSeeder for category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,19 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Category::create([
+            'title' => 'Technology',
+            'short_desc' => 'Exploring the cutting-edge realm of technology, where innovation and advancement converge.',
+        ]);
+
+        \App\Models\Category::create([
+            'title' => 'Food',
+            'short_desc' => 'Explore a delightful world of culinary excellence in our Food category.',
+        ]);
+        \App\Models\Category::create([
+            'title' => 'Music',
+            'short_desc' => 'Explore the vibrant world of music, where rhythm meets emotion.',
+        ]);
     }
 }
